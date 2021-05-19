4f49ccf5c8721548fb148a49a38a799cd2203b47
minilex <minilex.rtf@gmail.com>
2021-05-19 14:37:23 +0500

    Переименована WiegandProcessTx()

--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -211,7 +211,7 @@ void TIM6_IRQHandler(void)
   /* USER CODE END TIM6_IRQn 0 */
   HAL_TIM_IRQHandler(&htim6);
   /* USER CODE BEGIN TIM6_IRQn 1 */
-  WiegandProcess();
+  WiegandProcessTx();
   /* USER CODE END TIM6_IRQn 1 */
 }
 
